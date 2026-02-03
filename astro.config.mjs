69b15abc5e054db7b9293b7c3ea08859807748b0
Jack McNicol <jackmcpickle@gmail.com>
2026-02-03 17:24:04 +1030

    update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    env: {
+        schema: {
+            BUILDER_API_PUBLIC_KEY: envField.string({
+                context: 'client',
+                access: 'public',
+            }),
+        },
+    },
+    output: 'server',
+    trailingSlash: 'ignore',
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+    },
+    adapter: cloudflare({
+        imageService: 'compile',
+    }),
+});
---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly BUILDER_API_PUBLIC_KEY: string;
+    }
+}
